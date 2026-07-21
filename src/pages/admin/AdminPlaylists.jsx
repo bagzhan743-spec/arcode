dfb98fdb68977d9d5c90e79817f836c0b33421da
bagzhan743-spec <bagzhan743@gmail.com>
2026-07-22 01:20:13 +0500

    Add admin playlists page

---
+++ src/pages/admin/AdminPlaylists.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdminPlaylists = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-8">Playlists Management</h1>
+      <p className="text-gray-400">Playlists management coming soon</p>
+    </div>
+  )
+}
+
+export default AdminPlaylists
